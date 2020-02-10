788bb106445ae0052cf230863a4d1832da1463fa
kireev-ia <kireev.i.al@sberbank.ru>
2020-02-10 18:38:39 +0300

    * gender best params

--- opends/scenario_gender/bin/scenario_encoder_type.sh
+++ opends/scenario_gender/bin/scenario_encoder_type.sh
@@ -1,16 +1,4 @@
 # LSTM encoder
-export SC_SUFFIX="encoder_lstm"
-python metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.rnn.type="lstm" \
-    model_path.model="models/gender_mlm__$SC_SUFFIX.p" \
-    --conf conf/gender_dataset.hocon conf/gender_ml_params_train.json
-python ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/gender_mlm__$SC_SUFFIX.p" \
-    output.path="../data/gender/emb__$SC_SUFFIX" \
-    --conf conf/gender_dataset.hocon conf/gender_ml_params_inference.json
-
 export SC_SUFFIX="encoder_lstm_hs128"
 python metric_learning.py \
     params.device="$SC_DEVICE" \
@@ -56,19 +44,6 @@ python ml_inference.py \
 
 #
 # GRU encoder
-export SC_SUFFIX="encoder_gru"
-python metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.rnn.type="gru" \
-    model_path.model="models/gender_mlm__$SC_SUFFIX.p" \
-    --conf conf/gender_dataset.hocon conf/gender_ml_params_train.json
-python ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/gender_mlm__$SC_SUFFIX.p" \
-    output.path="../data/gender/emb__$SC_SUFFIX" \
-    --conf conf/gender_dataset.hocon conf/gender_ml_params_inference.json
-
-
 export SC_SUFFIX="encoder_gru_hs128"
 python metric_learning.py \
     params.device="$SC_DEVICE" \
@@ -259,11 +234,9 @@ python ml_inference.py \
 # Compare
 python -m scenario_gender compare_approaches --output_file "runs/scenario_gender__encoder_types.csv" \
     --skip_baseline --target_score_file_names --ml_embedding_file_names \
-    "emb__encoder_lstm.pickle"                               \
     "emb__encoder_lstm_hs128.pickle"                         \
     "emb__encoder_lstm_short.pickle"                         \
     "emb__encoder_lstm_long.pickle"                          \
-    "emb__encoder_gru.pickle"                                \
     "emb__encoder_gru_hs128.pickle"                          \
     "emb__encoder_gru_short.pickle"                          \
     "emb__encoder_gru_long.pickle"                           \
