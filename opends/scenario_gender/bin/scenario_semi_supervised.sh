da0c1c7b05f2f3c6d5929c35b1ca3f0e55a4b4eb
Nikita Ovsov <OvsovNikita@gmail.com>
2020-02-12 14:45:11 +0000

    scripts

--- opends/scenario_gender/bin/scenario_semi_supervised.sh
+++ opends/scenario_gender/bin/scenario_semi_supervised.sh
@@ -1,8 +1,9 @@
 for SC_AMOUNT in 378 756 1512 3024 6048
 do
 	python -m scenario_gender fit_target \
-        params.labeled_amount=$SC_AMOUNT \
         params.device="$SC_DEVICE" \
+        params.labeled_amount=$SC_AMOUNT \
+        params.train.n_epoch=20 \
         output.test.path="../data/gender/target_scores_$SC_AMOUNT"/test \
         output.valid.path="../data/gender/target_scores_$SC_AMOUNT"/valid \
         --conf conf/gender_dataset.hocon conf/gender_target_params_train.json
@@ -10,30 +11,31 @@ do
     python -m scenario_gender fit_finetuning \
         params.device="$SC_DEVICE" \
         params.labeled_amount=$SC_AMOUNT \
-        params.rnn.hidden_size=256 \
+        params.train.n_epoch=10 \
         output.test.path="../data/gender/finetuning_scores_$SC_AMOUNT"/test \
         output.valid.path="../data/gender/finetuning_scores_$SC_AMOUNT"/valid \
         --conf conf/gender_dataset.hocon conf/gender_finetuning_params_train.json
 
     python -m scenario_gender fit_finetuning \
-        params.labeled_amount=$SC_AMOUNT \
         params.device="$SC_DEVICE" \
-        params.rnn.hidden_size=256 \
+        params.labeled_amount=$SC_AMOUNT \
+        params.train.n_epoch=10 \
         params.pretrained_model_path="models/gender_cpc_model.p" \
         output.test.path="../data/gender/finetuning_cpc_scores_$SC_AMOUNT"/test \
         output.valid.path="../data/gender/finetuning_cpc_scores_$SC_AMOUNT"/valid \
         --conf conf/gender_dataset.hocon conf/gender_finetuning_params_train.json
 
     python -m scenario_gender pseudo_labeling \
-        params.labeled_amount=$SC_AMOUNT \
         params.device="$SC_DEVICE" \
+        params.labeled_amount=$SC_AMOUNT \
+        params.train.n_epoch=10 \
         output.test.path="../data/gender/pseudo_labeling_$SC_AMOUNT"/test \
         output.valid.path="../data/gender/pseudo_labeling_$SC_AMOUNT"/valid \
         --conf conf/gender_dataset.hocon conf/gender_pseudolabel_params_train.json
 
     Compare
     python -m scenario_gender compare_approaches \
-        --skip_emb_baselines --skip_linear --skip_xgboost 
+        --skip_emb_baselines --skip_linear --skip_xgboost \
         --target_score_file_names \
             target_scores_$SC_AMOUNT \
             finetuning_scores_$SC_AMOUNT \
