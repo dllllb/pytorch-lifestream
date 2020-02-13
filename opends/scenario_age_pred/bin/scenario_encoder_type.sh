0e99720e209205f3e77d70bc7d8a2e7fc6da636d
kireev-ia <kireev.i.al@sberbank.ru>
2020-02-13 12:54:51 +0300

    * change encoder types

--- opends/scenario_age_pred/bin/scenario_encoder_type.sh
+++ opends/scenario_age_pred/bin/scenario_encoder_type.sh
@@ -1,23 +1,8 @@
 # LSTM encoder
-export SC_SUFFIX="encoder_lstm_hs128"
-python metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.rnn.type="lstm" \
-    params.rnn.hidden_size=128 \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
-python ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    output.path="../data/age-pred/emb__$SC_SUFFIX" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
-
 export SC_SUFFIX="encoder_lstm_short"
 python metric_learning.py \
     params.device="$SC_DEVICE" \
     params.rnn.type="lstm" \
-    params.train.split_strategy.cnt_min=30 \
-    params.train.split_strategy.cnt_max=150 \
     model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
     --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
 python ml_inference.py \
@@ -25,7 +10,6 @@ python ml_inference.py \
     model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
     output.path="../data/age-pred/emb__$SC_SUFFIX" \
     --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
-
 
 export SC_SUFFIX="encoder_lstm_long"
 python metric_learning.py \
@@ -41,28 +25,12 @@ python ml_inference.py \
     output.path="../data/age-pred/emb__$SC_SUFFIX" \
     --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
 
-
 #
 # GRU encoder
-export SC_SUFFIX="encoder_gru_hs128"
-python metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.rnn.type="gru" \
-    params.rnn.hidden_size=128 \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
-python ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    output.path="../data/age-pred/emb__$SC_SUFFIX" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
-
 export SC_SUFFIX="encoder_gru_short"
 python metric_learning.py \
     params.device="$SC_DEVICE" \
     params.rnn.type="gru" \
-    params.train.split_strategy.cnt_min=30 \
-    params.train.split_strategy.cnt_max=150 \
     model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
     --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
 python ml_inference.py \
@@ -86,105 +54,12 @@ python ml_inference.py \
     --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
 #
 #
+
 # Transformer encoder
-export SC_SUFFIX="encoder_transf_bs512_2head_064hs_4layers"
+export SC_SUFFIX="encoder_transf_bs064_8head_128hs_4layers"
 python metric_learning.py \
     params.device="$SC_DEVICE" \
     params.model_type="transf" \
-    params.train.split_strategy.cnt_max=150 \
-    params.transf.n_heads=2 \
-    params.transf.input_size=64 \
-    params.transf.dim_hidden=64 \
-    params.transf.n_layers=4 \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
-python ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    params.valid.batch_size=128 \
-    output.path="../data/age-pred/emb__$SC_SUFFIX" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
-
-export SC_SUFFIX="encoder_transf_bs512_2head_032hs_4layers"
-python metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.model_type="transf" \
-    params.train.split_strategy.cnt_max=150 \
-    params.transf.n_heads=2 \
-    params.transf.input_size=32 \
-    params.transf.dim_hidden=32 \
-    params.transf.n_layers=4 \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
-python ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    params.valid.batch_size=128 \
-    output.path="../data/age-pred/emb__$SC_SUFFIX" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
-
-export SC_SUFFIX="encoder_transf_bs512_2head_064hs_2layers"
-python metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.model_type="transf" \
-    params.train.split_strategy.cnt_max=150 \
-    params.transf.n_heads=2 \
-    params.transf.input_size=64 \
-    params.transf.dim_hidden=64 \
-    params.transf.n_layers=2 \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
-python ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    params.valid.batch_size=128 \
-    output.path="../data/age-pred/emb__$SC_SUFFIX" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
-
-export SC_SUFFIX="encoder_transf_bs512_2head_032hs_2layers"
-python metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.model_type="transf" \
-    params.train.split_strategy.cnt_max=150 \
-    params.transf.n_heads=2 \
-    params.transf.input_size=32 \
-    params.transf.dim_hidden=32 \
-    params.transf.n_layers=2 \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
-python ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    params.valid.batch_size=128 \
-    output.path="../data/age-pred/emb__$SC_SUFFIX" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
-
-export SC_SUFFIX="encoder_transf_bs256_4head_128hs_4layers"
-python metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.model_type="transf" \
-    params.train.split_strategy.cnt_max=150 \
-    params.train.batch_size=256 \
-    params.transf.n_heads=4 \
-    params.transf.input_size=128 \
-    params.transf.dim_hidden=128 \
-    params.transf.n_layers=4 \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
-python ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    params.valid.batch_size=128 \
-    output.path="../data/age-pred/emb__$SC_SUFFIX" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
-
-export SC_SUFFIX="encoder_transf_bs256_8head_128hs_4layers_shared"
-python metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.model_type="transf" \
-    params.train.split_strategy.cnt_max=150 \
-    params.train.batch_size=256 \
-    params.transf.shared_layers=true \
     params.transf.n_heads=8 \
     params.transf.input_size=128 \
     params.transf.dim_hidden=128 \
@@ -198,115 +73,34 @@ python ml_inference.py \
     output.path="../data/age-pred/emb__$SC_SUFFIX" \
     --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
 
-export SC_SUFFIX="encoder_transf_bs256_4head_128hs_4layers_shared"
+export SC_SUFFIX="encoder_transf_bs064_8head_128hs_6layers"
 python metric_learning.py \
     params.device="$SC_DEVICE" \
     params.model_type="transf" \
-    params.train.split_strategy.cnt_max=150 \
-    params.train.batch_size=256 \
-    params.transf.shared_layers=true \
-    params.transf.n_heads=4 \
+    params.transf.n_heads=8 \
     params.transf.input_size=128 \
     params.transf.dim_hidden=128 \
-    params.transf.n_layers=4 \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
-python ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    params.valid.batch_size=128 \
-    output.path="../data/age-pred/emb__$SC_SUFFIX" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
-
-export SC_SUFFIX="encoder_transf_bs256_2head_128hs_4layers"
-python metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.model_type="transf" \
-    params.train.split_strategy.cnt_max=150 \
-    params.train.batch_size=256 \
-    params.transf.n_heads=2 \
-    params.transf.input_size=128 \
-    params.transf.dim_hidden=128 \
-    params.transf.n_layers=4 \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
-python ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    params.valid.batch_size=128 \
-    output.path="../data/age-pred/emb__$SC_SUFFIX" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
-
-export SC_SUFFIX="encoder_transf_bs256_4head_064hs_4layers"
-python metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.model_type="transf" \
-    params.train.split_strategy.cnt_max=150 \
-    params.train.batch_size=256 \
-    params.transf.n_heads=4 \
-    params.transf.input_size=64 \
-    params.transf.dim_hidden=64 \
-    params.transf.n_layers=4 \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
-python ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    params.valid.batch_size=128 \
-    output.path="../data/age-pred/emb__$SC_SUFFIX" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
-
-export SC_SUFFIX="encoder_transf_bs256_4head_128hs_2layers"
-python metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.model_type="transf" \
-    params.train.split_strategy.cnt_max=150 \
-    params.train.batch_size=256 \
-    params.transf.n_heads=4 \
-    params.transf.input_size=128 \
-    params.transf.dim_hidden=128 \
-    params.transf.n_layers=2 \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
-python ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    params.valid.batch_size=128 \
-    output.path="../data/age-pred/emb__$SC_SUFFIX" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
-
-export SC_SUFFIX="encoder_transf_bs128_4head_196hs_4layers"
-python metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.model_type="transf" \
-    params.train.split_strategy.cnt_max=150 \
-    params.train.batch_size=128 \
-    params.transf.n_heads=4 \
-    params.transf.input_size=196 \
-    params.transf.dim_hidden=196 \
-    params.transf.n_layers=4 \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
-python ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
-    params.valid.batch_size=128 \
-    output.path="../data/age-pred/emb__$SC_SUFFIX" \
-    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
-
-export SC_SUFFIX="encoder_transf_bs128_4head_196hs_6layers"
-python metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.model_type="transf" \
-    params.train.split_strategy.cnt_max=150 \
-    params.train.batch_size=128 \
-    params.transf.n_heads=4 \
-    params.transf.input_size=196 \
-    params.transf.dim_hidden=196 \
     params.transf.n_layers=6 \
     model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
     --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
-export SC_SUFFIX="encoder_transf_bs128_4head_196hs_6layers"
+python ml_inference.py \
+    params.device="$SC_DEVICE" \
+    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
+    params.valid.batch_size=128 \
+    output.path="../data/age-pred/emb__$SC_SUFFIX" \
+    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_inference.json
+
+export SC_SUFFIX="encoder_transf_bs256_4head_128hs_4layers"
+python metric_learning.py \
+    params.device="$SC_DEVICE" \
+    params.model_type="transf" \
+    params.train.batch_size=256 \
+    params.transf.n_heads=4 \
+    params.transf.input_size=128 \
+    params.transf.dim_hidden=128 \
+    params.transf.n_layers=4 \
+    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
+    --conf conf/age_pred_dataset.hocon conf/age_pred_ml_params_train.json
 python ml_inference.py \
     params.device="$SC_DEVICE" \
     model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
@@ -318,21 +112,14 @@ python ml_inference.py \
 # Compare
 python -m scenario_age_pred compare_approaches --output_file "runs/scenario_age_pred__encoder_types.csv" \
     --skip_baseline --target_score_file_names --ml_embedding_file_names \
-    "emb__encoder_lstm_hs128.pickle"                              \
     "emb__encoder_lstm_short.pickle"                              \
     "emb__encoder_lstm_long.pickle"                               \
-    "emb__encoder_gru_hs128.pickle"                               \
     "emb__encoder_gru_short.pickle"                               \
     "emb__encoder_gru_long.pickle"                                \
-    "emb__encoder_transf_bs512_2head_064hs_4layers.pickle"        \
-    "emb__encoder_transf_bs512_2head_032hs_4layers.pickle"        \
-    "emb__encoder_transf_bs512_2head_064hs_2layers.pickle"        \
-    "emb__encoder_transf_bs512_2head_032hs_2layers.pickle"        \
-    "emb__encoder_transf_bs256_4head_128hs_4layers.pickle"        \
-    "emb__encoder_transf_bs256_4head_128hs_4layers_shared.pickle" \
-    "emb__encoder_transf_bs256_2head_128hs_4layers.pickle"        \
-    "emb__encoder_transf_bs256_4head_064hs_4layers.pickle"        \
-    "emb__encoder_transf_bs256_4head_128hs_2layers.pickle"        \
-    "emb__encoder_transf_bs128_4head_196hs_4layers.pickle"        \
-    "emb__encoder_transf_bs128_4head_196hs_6layers.pickle"
+    "emb__encoder_transf_bs064_8head_128hs_4layers.pickle"        \
+    "emb__encoder_transf_bs064_8head_128hs_6layers.pickle"        \
+    "emb__encoder_transf_bs256_4head_128hs_4layers.pickle"
+
+
+
 
