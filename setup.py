630f4a263076402bb8e3213f91de9a6da1a25377
Dmitri Babaev <dmitri.babaev@gmail.com>
2020-03-03 17:46:17 +0300

    dependencies

--- setup.py
+++ setup.py
@@ -8,5 +8,13 @@ setup(
     description='Llifestream data anaysis with PyTorch',
     author='Dmitri Babaev',
     author_email='dmitri.babaev@gmail.com',
-    install_requires=['torch>=1.1.0', 'pytorch-ignite>=0.2.1']
+    install_requires=[
+        'torch>=1.1.0',
+        'pytorch-ignite>=0.2.1',
+        'scikit-learn>=0.21.2',
+	'numpy>=1.16.4',
+	'pandas>=0.24.2',
+        'scipy>=1.3.0',
+        'tqdm>=4.32.2',
+        'tensorboard>=2.1.0']
 )
