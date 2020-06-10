903fdf3002be75703178f15511f7a7e9fc0fb016
Ivan A Kireev <ivkireev@yandex.ru>
2020-06-10 13:43:50 +0300

    fix histogram loss

--- dltranz/metric_learn/losses.py
+++ dltranz/metric_learn/losses.py
@@ -140,7 +140,7 @@ class HistogramLoss(torch.nn.Module):
         neg_size = (~classes_eq[s_inds]).sum().item()
         s = dists[s_inds].view(1, -1)
         s_repeat = s.repeat(self.tsize, 1)
-        s_repeat_floor = (torch.floor((s_repeat.data - 1e-6) / self.step) * self.step).float()
+        s_repeat_floor = (torch.floor((s_repeat.data + 1.0 - 1e-6) / self.step) * self.step - 1.0).float()
         
         histogram_pos = histogram(pos_inds, pos_size)
         assert_almost_equal(histogram_pos.sum().item(), 1, decimal=1, 
