71a89422bbaa03614965ed303dd53f597db11d1d
Ivan A Kireev <ivkireev@yandex.ru>
2020-06-10 12:03:03 +0300

    scenario_x5__hidden_size.csv

--- experiments/scenario_x5/bin/scenario_hidden_size.sh
+++ experiments/scenario_x5/bin/scenario_hidden_size.sh
@@ -1,9 +1,6 @@
-export SC_EPOCH_N=1
-
 export SC_SUFFIX="hidden_size__hs_0064"
 python ../../metric_learning.py \
     params.device="$SC_DEVICE" \
-    params.train.n_epoch=$SC_EPOCH_N \
     params.rnn.hidden_size=64 \
     model_path.model="models/x5_mlm__$SC_SUFFIX.p" \
     --conf "conf/dataset.hocon" "conf/mles_params.json"
@@ -16,7 +13,6 @@ python ../../ml_inference.py \
 export SC_SUFFIX="hidden_size__hs_0160"
 python ../../metric_learning.py \
     params.device="$SC_DEVICE" \
-    params.train.n_epoch=$SC_EPOCH_N \
     params.rnn.hidden_size=160 \
     model_path.model="models/x5_mlm__$SC_SUFFIX.p" \
     --conf "conf/dataset.hocon" "conf/mles_params.json"
@@ -29,8 +25,7 @@ python ../../ml_inference.py \
 export SC_SUFFIX="hidden_size__hs_0480"
 python ../../metric_learning.py \
     params.device="$SC_DEVICE" \
-    params.train.n_epoch=$SC_EPOCH_N \
-    params.train.batch_size=128 \
+    params.train.batch_size=256 \
     params.rnn.hidden_size=480 \
     model_path.model="models/x5_mlm__$SC_SUFFIX.p" \
     --conf "conf/dataset.hocon" "conf/mles_params.json"
@@ -43,8 +38,7 @@ python ../../ml_inference.py \
 export SC_SUFFIX="hidden_size__hs_0800"
 python ../../metric_learning.py \
     params.device="$SC_DEVICE" \
-    params.train.n_epoch=$SC_EPOCH_N \
-    params.train.batch_size=64 \
+    params.train.batch_size=256 \
     params.rnn.hidden_size=800 \
     model_path.model="models/x5_mlm__$SC_SUFFIX.p" \
     --conf "conf/dataset.hocon" "conf/mles_params.json"
@@ -56,9 +50,8 @@ python ../../ml_inference.py \
     
 export SC_SUFFIX="hidden_size__hs_1600"
 python ../../metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.train.n_epoch=$SC_EPOCH_N \
-    params.train.batch_size=64 \
+    params.device="$SC_DEVICE"  \
+    params.train.batch_size=64  \
     params.rnn.hidden_size=1600 \
     model_path.model="models/x5_mlm__$SC_SUFFIX.p" \
     --conf "conf/dataset.hocon" "conf/mles_params.json"
@@ -70,7 +63,7 @@ python ../../ml_inference.py \
 
 # Compare
 python -m scenario_x5 compare_approaches --output_file "results/scenario_x5__hidden_size.csv" \
-    --embedding_file_names \
+    --n_workers 3 --models lgb --embedding_file_names \
     "emb__hidden_size__hs_1600.pickle" \
     "emb__hidden_size__hs_0800.pickle"  \
     "emb__hidden_size__hs_0480.pickle"  \
