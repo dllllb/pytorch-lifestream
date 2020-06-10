99d6972f27bd50c8b937221625f27b3369483c2a
Nikita Ovsov <OvsovNikita@gmail.com>
2020-06-10 16:15:02 +0000

    fixes, refactoring, cosmetic

--- experiments/scenario_bowl2019/bin/scenario_hidden_size.sh
+++ experiments/scenario_bowl2019/bin/scenario_hidden_size.sh
@@ -1,45 +1,70 @@
-export SC_SUFFIX="hidden_size_0160"
+export SC_SUFFIX="hidden_size_0032"
 python ../../metric_learning.py \
     params.device="$SC_DEVICE" \
-    params.rnn.hidden_size=160 \
+    params.rnn.hidden_size=32 \
     model_path.model="models/bowl2019_mlm__$SC_SUFFIX.p" \
-    --conf "conf/dataset.hocon" "conf/mles_params.json"
+    --conf "conf/trx_dataset.hocon" "conf/mles_params.json"
 python ../../ml_inference.py \
     params.device="$SC_DEVICE" \
     model_path.model="models/bowl2019_mlm__$SC_SUFFIX.p" \
     output.path="data/emb__$SC_SUFFIX" \
     --conf "conf/dataset.hocon" "conf/mles_params.json"
 
-export SC_SUFFIX="hidden_size_0080"
+export SC_SUFFIX="hidden_size_0064"
 python ../../metric_learning.py \
     params.device="$SC_DEVICE" \
-    params.rnn.hidden_size=80 \
+    params.rnn.hidden_size=64 \
     model_path.model="models/bowl2019_mlm__$SC_SUFFIX.p" \
-    --conf "conf/dataset.hocon" "conf/mles_params.json"
+    --conf "conf/trx_dataset.hocon" "conf/mles_params.json"
 python ../../ml_inference.py \
     params.device="$SC_DEVICE" \
     model_path.model="models/bowl2019_mlm__$SC_SUFFIX.p" \
     output.path="data/emb__$SC_SUFFIX" \
     --conf "conf/dataset.hocon" "conf/mles_params.json"
 
-export SC_SUFFIX="hidden_size_0040"
+
+export SC_SUFFIX="hidden_size_0100"
 python ../../metric_learning.py \
     params.device="$SC_DEVICE" \
-    params.rnn.hidden_size=40 \
+    params.rnn.hidden_size=100 \
     model_path.model="models/bowl2019_mlm__$SC_SUFFIX.p" \
-    --conf "conf/dataset.hocon" "conf/mles_params.json"
+    --conf "conf/trx_dataset.hocon" "conf/mles_params.json"
 python ../../ml_inference.py \
     params.device="$SC_DEVICE" \
     model_path.model="models/bowl2019_mlm__$SC_SUFFIX.p" \
     output.path="data/emb__$SC_SUFFIX" \
     --conf "conf/dataset.hocon" "conf/mles_params.json"
 
-export SC_SUFFIX="hidden_size_0020"
+export SC_SUFFIX="hidden_size_0200"
 python ../../metric_learning.py \
     params.device="$SC_DEVICE" \
-    params.rnn.hidden_size=20 \
+    params.rnn.hidden_size=200 \
     model_path.model="models/bowl2019_mlm__$SC_SUFFIX.p" \
+    --conf "conf/trx_dataset.hocon" "conf/mles_params.json"
+python ../../ml_inference.py \
+    params.device="$SC_DEVICE" \
+    model_path.model="models/bowl2019_mlm__$SC_SUFFIX.p" \
+    output.path="data/emb__$SC_SUFFIX" \
     --conf "conf/dataset.hocon" "conf/mles_params.json"
+
+export SC_SUFFIX="hidden_size_0400"
+python ../../metric_learning.py \
+    params.device="$SC_DEVICE" \
+    params.rnn.hidden_size=400 \
+    model_path.model="models/bowl2019_mlm__$SC_SUFFIX.p" \
+    --conf "conf/trx_dataset.hocon" "conf/mles_params.json"
+python ../../ml_inference.py \
+    params.device="$SC_DEVICE" \
+    model_path.model="models/bowl2019_mlm__$SC_SUFFIX.p" \
+    output.path="data/emb__$SC_SUFFIX" \
+    --conf "conf/dataset.hocon" "conf/mles_params.json"
+
+export SC_SUFFIX="hidden_size_0800"
+python ../../metric_learning.py \
+    params.device="$SC_DEVICE" \
+    params.rnn.hidden_size=800 \
+    model_path.model="models/bowl2019_mlm__$SC_SUFFIX.p" \
+    --conf "conf/trx_dataset.hocon" "conf/mles_params.json"
 python ../../ml_inference.py \
     params.device="$SC_DEVICE" \
     model_path.model="models/bowl2019_mlm__$SC_SUFFIX.p" \
@@ -49,8 +74,10 @@ python ../../ml_inference.py \
 # Compare
 python -m scenario_bowl2019 compare_approaches --output_file "results/scenario_bowl2019__hidden_size.csv" \
     --embedding_file_names \
-    "emb__hidden_size_0160.pickle" \
-    "emb__hidden_size_0080.pickle" \
-    "emb__hidden_size_0040.pickle" \
-    "emb__hidden_size_0020.pickle"
-            
+    "emb__hidden_size_0032.pickle" \
+    "emb__hidden_size_0064.pickle" \
+    "emb__hidden_size_0100.pickle" \
+    "emb__hidden_size_0200.pickle" \
+    "emb__hidden_size_0400.pickle" \
+    "emb__hidden_size_0800.pickle" 
+
--- experiments/scenario_bowl2019/bin/scenario_semi_supervised.sh
+++ experiments/scenario_bowl2019/bin/scenario_semi_supervised.sh
@@ -1,5 +1,5 @@
 
-for SC_AMOUNT in 496, 992, 1985, 3971, 7943, 15887
+for SC_AMOUNT in 496 992 1985 3971 7943
 do
 	python -m scenario_bowl2019 fit_target \
         params.device="$SC_DEVICE" \
@@ -11,7 +11,6 @@ do
     python -m scenario_bowl2019 fit_finetuning \
         params.device="$SC_DEVICE" \
         params.labeled_amount=$SC_AMOUNT \
-        params.pretrained_model_path="models/bowl2019_ml_model_ft.p" \
         output.test.path="data/mles_finetuning_scores_$SC_AMOUNT"/test \
         output.valid.path="data/mles_finetuning_scores_$SC_AMOUNT"/valid \
         --conf "conf/dataset.hocon" conf/fit_finetuning_on_mles_params.json
@@ -19,17 +18,9 @@ do
     python -m scenario_bowl2019 fit_finetuning \
         params.device="$SC_DEVICE" \
         params.labeled_amount=$SC_AMOUNT \
-        params.pretrained_model_path="models/cpc_model.p" \
         output.test.path="data/cpc_finetuning_scores_$SC_AMOUNT"/test \
         output.valid.path="data/cpc_finetuning_scores_$SC_AMOUNT"/valid \
         --conf "conf/dataset.hocon" conf/fit_finetuning_on_cpc_params.json
-
-    python -m scenario_bowl2019 pseudo_labeling \
-        params.device="$SC_DEVICE" \
-        params.labeled_amount=$SC_AMOUNT \
-        output.test.path="data/pseudo_labeling_$SC_AMOUNT"/test \
-        output.valid.path="data/pseudo_labeling_$SC_AMOUNT"/valid \
-        --conf "conf/dataset.hocon" conf/pseudolabel_params.json
 
     # Compare
     python -m scenario_bowl2019 compare_approaches \
@@ -38,10 +29,9 @@ do
             target_scores_$SC_AMOUNT \
             mles_finetuning_scores_$SC_AMOUNT \
             cpc_finetuning_scores_$SC_AMOUNT \
-            pseudo_labeling_$SC_AMOUNT \
         --labeled_amount $SC_AMOUNT \
-        --output_file results/semi_scenario_bowl2019_$SC_AMOUNT.csv \
-        --embedding_file_names "embeddings.pickle" "embeddings_cpc.pickle"
+        --output_file results/semi_scenario_bowl2019_$SC_AMOUNT.csv
+
 done
 
 
