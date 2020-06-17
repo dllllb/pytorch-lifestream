91f209d78436138893fde993074cf2393865a6bd
Nikita Ovsov <OvsovNikita@gmail.com>
2020-06-17 13:58:15 +0300

    mutliprocessing

--- tests/dltranz_tests/test_size_reduction.py
+++ tests/dltranz_tests/test_size_reduction.py
@@ -26,10 +26,16 @@ def test_embeds_quantiles():
         (3, 3, 3, 0),
     ])
 
-    q_embeds, bins = embeds_quantiles(array, q_count=4)
+    q_embeds, bins = embeds_quantiles(array, q_count=4, num_workers=0)
     assert np.array_equal(q_embeds, out)
 
-    q_embeds = embeds_quantiles(array, bins=bins)
+    q_embeds = embeds_quantiles(array, bins=bins, num_workers=0)
+    assert np.array_equal(q_embeds, out)
+
+    q_embeds, bins = embeds_quantiles(array, q_count=4, num_workers=8)
+    assert np.array_equal(q_embeds, out)
+
+    q_embeds = embeds_quantiles(array, bins=bins, num_workers=8)
     assert np.array_equal(q_embeds, out)
 
 
@@ -47,8 +53,14 @@ def test_pca_reduction():
 
     out = np.array([[-0.3, -0.1,  0.1,  0.3, -0.3, -0.1,  0.1,  0.3]]).T
 
-    reduced_array, pca_list = pca_reduction(array, sub_dim=4, n_components=1)
+    reduced_array, pca_list = pca_reduction(array, sub_dim=4, n_components=1, num_workers=0)
     assert np.allclose(reduced_array, out, rtol=1e-07)
 
-    reduced_array = pca_reduction(array, sub_dim=4, pca_list=pca_list)
+    reduced_array = pca_reduction(array, sub_dim=4, pca_list=pca_list, num_workers=0)
+    assert np.allclose(reduced_array, out, rtol=1e-07)
+
+    reduced_array, pca_list = pca_reduction(array, sub_dim=4, n_components=1, num_workers=8)
+    assert np.allclose(reduced_array, out, rtol=1e-07)
+
+    reduced_array = pca_reduction(array, sub_dim=4, pca_list=pca_list, num_workers=8)
     assert np.allclose(reduced_array, out, rtol=1e-07)
