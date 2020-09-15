acdee10712f404a6dd68cd765e838c0f557ab599
BearSubj13 <bearsubj13@yandex.ru>
2020-09-15 14:20:51 +0000

    scencario_spend_only_rur folder added

---
+++ experiments/scenario_spend_only_rur/bin/scenario_sampling_strategy.sh
@@ -0,0 +1,34 @@
+# AllPositivePair
+export SC_SUFFIX="smpl_strategy_AllPositivePair"
+python ../../metric_learning.py \
+    params.device="$SC_DEVICE" \
+    params.train.sampling_strategy="AllPositivePair" \
+    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
+    --conf "conf/dataset.hocon" "conf/mles_params.json"
+python ../../ml_inference.py \
+    params.device="$SC_DEVICE" \
+    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
+    output.path="data/emb__$SC_SUFFIX" \
+    --conf "conf/dataset.hocon" "conf/mles_params.json"
+
+
+# DistanceWeightedPair
+export SC_SUFFIX="smpl_strategy_DistanceWeightedPair"
+python ../../metric_learning.py \
+    params.device="$SC_DEVICE" \
+    params.train.sampling_strategy="DistanceWeightedPair" \
+    params.train.n_samples_from_class=5 \
+    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
+    --conf "conf/dataset.hocon" "conf/mles_params.json"
+python ../../ml_inference.py \
+    params.device="$SC_DEVICE" \
+    model_path.model="models/age_pred_mlm__$SC_SUFFIX.p" \
+    output.path="data/emb__$SC_SUFFIX" \
+    --conf "conf/dataset.hocon" "conf/mles_params.json"
+
+
+# Compare
+python -m scenario_age_pred compare_approaches --output_file "results/scenario_age_pred__smpl_strategy.csv" \
+    --embedding_file_names \
+    "emb__smpl_strategy_AllPositivePair.pickle"                     \
+    "emb__smpl_strategy_DistanceWeightedPair.pickle"
