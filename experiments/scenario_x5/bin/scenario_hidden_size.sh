215b7f95c3f858eeb2dfd0422204d84de66e1747
Ivan A Kireev <ivkireev@yandex.ru>
2020-09-30 06:25:05 +0000

    x5 hidden

--- experiments/scenario_x5/bin/scenario_hidden_size.sh
+++ experiments/scenario_x5/bin/scenario_hidden_size.sh
@@ -13,6 +13,22 @@ do
       --conf "conf/dataset.hocon" "conf/mles_params.json"
 done
 
+for SC_HIDDEN_SIZE in 1600
+do
+  export SC_SUFFIX="hidden_size__hs_${SC_HIDDEN_SIZE}"
+  python ../../metric_learning.py \
+      params.device="$SC_DEVICE" \
+      params.rnn.hidden_size=${SC_HIDDEN_SIZE} \
+      params.train.batch_size=128 \
+      model_path.model="models/x5_mlm__$SC_SUFFIX.p" \
+      --conf "conf/dataset.hocon" "conf/mles_params.json"
+  python ../../ml_inference.py \
+      params.device="$SC_DEVICE" \
+      model_path.model="models/x5_mlm__$SC_SUFFIX.p" \
+      output.path="data/emb__$SC_SUFFIX" \
+      --conf "conf/dataset.hocon" "conf/mles_params.json"
+done
+
 # Compare
 python -m scenario_x5 compare_approaches --output_file "results/scenario_x5__hidden_size.csv" \
     --n_workers 3 --models lgb --embedding_file_names \
