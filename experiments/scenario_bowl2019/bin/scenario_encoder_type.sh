a779c09493ac21c475a9e171635b1a271c172381
Ivan A Kireev <ivkireev@yandex.ru>
2020-10-01 14:35:39 +0300

    switch_reproducibility_on

--- experiments/scenario_bowl2019/bin/scenario_encoder_type.sh
+++ experiments/scenario_bowl2019/bin/scenario_encoder_type.sh
@@ -48,28 +48,6 @@ python ../../ml_inference.py \
     --conf "conf/dataset.hocon" "conf/mles_params.json"
 
 # Transformer encoder
-export SC_SUFFIX="encoder_transf_bs064_8head_64hs_4layers"
-python ../../metric_learning.py \
-    params.device="$SC_DEVICE" \
-    params.model_type="transf" \
-    params.transf.n_heads=8 \
-    params.transf.n_layers=4 \
-    params.train.batch_size=64 \
-    params.valid.batch_size=64 \
-    params.train.split_strategy.cnt_min=50 \
-    params.train.split_strategy.cnt_max=200 \
-    params.valid.split_strategy.cnt_min=50 \
-    params.valid.split_strategy.cnt_max=200 \
-    model_path.model="models/bowl2019_mlm__$SC_SUFFIX.p" \
-    --conf "conf/trx_dataset.hocon" "conf/mles_params.json"
-python ../../ml_inference.py \
-    params.device="$SC_DEVICE" \
-    model_path.model="models/bowl2019_mlm__$SC_SUFFIX.p" \
-    params.valid.batch_size=32 \
-    output.path="data/emb__$SC_SUFFIX" \
-    --conf "conf/dataset.hocon" "conf/mles_params.json"
-
-# Transformer encoder
 export SC_SUFFIX="encoder_transf_bs064_4head_64hs_8layers"
 python ../../metric_learning.py \
     params.device="$SC_DEVICE" \
@@ -98,7 +76,6 @@ python -m scenario_bowl2019 compare_approaches --models 'lgb' --output_file "res
     "emb__encoder_gru.pickle"                                \
     "emb__encoder_lstm.pickle"                               \
     "emb__encoder_transf_bs064_4head_64hs_4layers.pickle"         \
-    "emb__encoder_transf_bs064_8head_64hs_4layers.pickle"         \
     "emb__encoder_transf_bs064_4head_64hs_8layers.pickle"
 
 
