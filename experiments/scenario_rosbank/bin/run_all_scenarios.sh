9a7b9df4aa9a625da635e3948cd7ead6128387ab
Darkhan <nur.darhan@gmail.com>
2021-02-08 06:28:01 +0000

    rosbank

--- experiments/scenario_rosbank/bin/run_all_scenarios.sh
+++ experiments/scenario_rosbank/bin/run_all_scenarios.sh
@@ -1,18 +1,18 @@
-## start with working directory: experiments/scenario_gender
-## dataset should be prepared before this script
-# echo "==== Folds split"
-# rm -r lightning_logs/
-# rm -r conf/embeddings_validation.work/
-# python -m embeddings_validation \
-#     --conf conf/embeddings_validation_baselines_supervised.hocon --workers 10 --total_cpu_count 20 \
-#     --split_only
+# start with working directory: experiments/scenario_gender
+# dataset should be prepared before this script
+echo "==== Folds split"
+rm -r lightning_logs/
+rm -r conf/embeddings_validation.work/
+python -m embeddings_validation \
+    --conf conf/embeddings_validation_baselines_supervised.hocon --workers 10 --total_cpu_count 20 \
+    --split_only
 
-# echo "==== Device ${SC_DEVICE} will be used"
+echo "==== Device ${SC_DEVICE} will be used"
 
 
 echo ""
 echo "==== Main track"
-# sh bin/scenario_baselines_unsupervised.sh
+sh bin/scenario_baselines_unsupervised.sh
 sh bin/scenario_baselines_supervised.sh
 
 
