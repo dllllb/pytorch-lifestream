0b54dec1bfc66b261617bf0412ed308208b05de8
Dmitri Babaev <dmitri.babaev@gmail.com>
2022-02-04 16:51:54 +0300

    mandatory dependency

--- setup.py
+++ setup.py
@@ -19,5 +19,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
+    install_requires=[
+        'pytorch-lightning==1.3.*',
+    ],
 )
