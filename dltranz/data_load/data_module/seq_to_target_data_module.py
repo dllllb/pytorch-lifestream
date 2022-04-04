70d38984d861283e415c70680436b07703d0d0c9
justalge <43386960+justalge@users.noreply.github.com>
2022-04-04 12:54:02 +0300

    Docstring for SeqToTargetDatamodule

--- dltranz/data_load/data_module/seq_to_target_data_module.py
+++ dltranz/data_load/data_module/seq_to_target_data_module.py
@@ -12,27 +12,51 @@ from sklearn.model_selection import train_test_split
 
 
 class SeqToTargetDatamodule(pl.LightningDataModule):
+    r"""pytorch-lightning data module for supervised training
+    Parameters
+    ----------
+     dataset: List[Dict]
+        dataset
+     pl_module: pl.LightningModule
+        Pytorch-lightning module used for training seq_encoder.
+     min_seq_len: int. Default: 0.
+        The minimal length of sequences used for training. The shorter sequences would be skipped.
+     valid_size: float. Default: 0.05.
+        Should be between 0.0 and 1.0 and represent the proportion of the dataset to include in the test split.
+     train_num_workers: int. Default: 0.
+        The number of workers for train dataloader. 0 = single-process loader
+     train_batch_size: int. Default: 256.
+        The number of samples (before splitting to subsequences) in each batch during training.
+     valid_num_workers: int. Default: 0.
+        The number of workers for validation dataloader. 0 = single-process loader       
+     valid_batch_size: int. Default: 256.
+        The number of samples (before splitting to subsequences) in each batch during validation.
+     target_col: str. Default: 'target'.
+        The name of target column.
+     random_state : int. Default : 42.
+        Controls the shuffling applied to the data before applying the split.
+     """
     def __init__(self,
                  dataset: List[dict],
                  pl_module: pl.LightningModule,
                  min_seq_len: int = 0,
-                 val_size: float = 0.05,
+                 valid_size: float = 0.05,
                  train_num_workers: int = 0,
                  train_batch_size: int = 256,
-                 val_num_workers: int = 0,
-                 val_batch_size: int = 256,
-                 random_state: int = 42,
-                 target_col: str = 'target'):
+                 valid_num_workers: int = 0,
+                 valid_batch_size: int = 256,
+                 target_col: str = 'target',
+                 random_state: int = 42):
 
         super().__init__()
-        self.dataset_train, self.dataset_val = train_test_split(dataset,
-                                                                test_size=val_size,
-                                                                random_state=random_state)
+        self.dataset_train, self.dataset_valid = train_test_split(dataset,
+                                                                  test_size=valid_size,
+                                                                  random_state=random_state)
         self.min_seq_len=min_seq_len
         self.train_num_workers = train_num_workers
         self.train_batch_size = train_batch_size
-        self.val_num_workers = val_num_workers
-        self.val_batch_size = val_batch_size
+        self.valid_num_workers = valid_num_workers
+        self.valid_batch_size = valid_batch_size
         self.keep_features = pl_module.seq_encoder.category_names
         self.keep_features.add('event_time')
         self.category_max_size = pl_module.seq_encoder.category_max_size
@@ -41,7 +65,7 @@ class SeqToTargetDatamodule(pl.LightningDataModule):
 
     def prepare_data(self):
         self.dataset_train = list(self.post_proc(iter(self.dataset_train)))
-        self.dataset_val = list(self.post_proc(iter(self.dataset_val)))
+        self.dataset_valid = list(self.post_proc(iter(self.dataset_valid)))
 
     def build_iterable_processing(self):
         yield SeqLenFilter(min_seq_len=self.min_seq_len)
