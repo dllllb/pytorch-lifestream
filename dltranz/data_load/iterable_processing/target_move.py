70835ec21a8fc2d1a868754d6aafc342cb1034e0
justalge <43386960+justalge@users.noreply.github.com>
2022-04-05 13:10:52 +0300

    Update target_move.py

--- dltranz/data_load/iterable_processing/target_move.py
+++ dltranz/data_load/iterable_processing/target_move.py
@@ -2,7 +2,17 @@ from dltranz.data_load.iterable_processing_dataset import IterableProcessingData
 
 
 class TargetMove(IterableProcessingDataset):
-    def __init__(self, target_col):
+    """Filter that takes target from sample dict and place it in tuple with dict
+    Parameters
+     ----------
+    target_col : str. Default: 'target'
+         Name of target columns in sample dict.
+         
+    Filter transformation:
+    list(input_dict) -> list((input_dict, target_val))
+    (in place of list there could be other iterable, for example generator)
+    """
+    def __init__(self, target_col='target'):
         super().__init__()
         self.target_col = target_col
 
