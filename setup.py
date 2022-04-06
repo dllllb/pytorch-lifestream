3cc3868c9a9a4a3c0842233ef9e6360ecf02d9e6
Dmitri Babaev <dmitri.babaev@gmail.com>
2022-04-06 14:36:40 +0000

    v0.1.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name='pytorch-lifestream',
-    version='0.1.2',
+    version='0.1.3',
     author='',
     author_email='',
     description='Lifestream data analysis with PyTorch',
