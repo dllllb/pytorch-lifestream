01c8967fa568e0c3fcf218f748c7fc556e885b31
Dmitri Babaev <dmitri.babaev@gmail.com>
2022-04-26 22:50:42 +0000

    test fixes

--- tests/dltranz_tests/test_pl_api.py
+++ tests/dltranz_tests/test_pl_api.py
@@ -1,22 +1,36 @@
 import torch
 import pytorch_lightning as pl
+import pandas as pd
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
+from dltranz.data_preprocessing.pandas_preprocessor import PandasDataPreprocessor
 from dltranz.seq_encoder import SequenceEncoder
 from dltranz.models import Head
 from dltranz.lightning_modules.emb_module import EmbModule
 from dltranz.data_load.data_module.emb_data_module import EmbeddingDatasetFactory
 
-from tests.dltranz_tests.test_data_load import RandomEventData, gen_trx_data
-
 
 def test_train_inference():
-    cat_sizes = {
-        'mcc_code': 21,
-        'trans_type': 11,
-    }
+    source_data = pd.read_csv(Path(__file__).parent / "age-transactions.csv")
+
+    preprocessor = PandasDataPreprocessor(
+        col_id='client_id',
+        cols_event_time='trans_date',
+        time_transformation='float',
+        cols_category=["trans_date", "small_group"],
+        cols_log_norm=["amount_rur"],
+        cols_identity=[],
+        print_dataset_info=False,
+    )
+
+    dataset = preprocessor.fit_transform(source_data)
+
+    train, test = train_test_split(dataset, test_size=0.4, random_state=42)
 
     seq_encoder = SequenceEncoder(
-        category_features=cat_sizes,
+        category_features=preprocessor.get_category_sizes(),
         numeric_features=[],
         trx_embedding_noize=0.003
     )
@@ -26,26 +40,22 @@ def test_train_inference():
     model = EmbModule(seq_encoder=seq_encoder, head=head)
 
     dlf = EmbeddingDatasetFactory(
-        min_seq_len=25,
+        min_seq_len=5,
         seq_split_strategy='SampleSlices',
         category_names = model.seq_encoder.category_names,
         category_max_size = model.seq_encoder.category_max_size,
         split_count=5,
-        split_cnt_min=25,
-        split_cnt_max=200,
+        split_cnt_min=5,
+        split_cnt_max=20,
     )
-
-    train = gen_trx_data((torch.rand(1000)*60+1).long())
 
     trainer = pl.Trainer(
         max_epochs=1,
         gpus=1 if torch.cuda.is_available() else 0
     )
 
-    train_dl = dlf.train_data_loader(train, num_workers=1, batch_size=8)
+    train_dl = dlf.train_data_loader(train, num_workers=1, batch_size=4)
     trainer.fit(model, train_dl)
 
-    test = gen_trx_data((torch.rand(1000)*60+1).long())
-
-    test_dl = dlf.inference_data_loader(test, num_workers=1, batch_size=8)
+    test_dl = dlf.inference_data_loader(test, num_workers=1, batch_size=4)
     trainer.predict(model, test_dl)
