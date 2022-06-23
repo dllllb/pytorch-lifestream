7171d3a1a388bea6ee04182248d780708664f134
Ivan A Kireev <ivkireev@yandex.ru>
2022-06-23 16:14:27 +0300

    python to 3.7

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.7',
     install_requires=[
         'pytorch-lightning==1.5.*',
         'pyarrow==7.*',
