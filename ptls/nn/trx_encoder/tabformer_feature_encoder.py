ae3c4dd04a3f6be128342d58dcd8e4b22869f960
justalge <miheevalge@gmail.com>
2022-09-20 15:43:56 +0000

    no sep token in feature encoder

--- ptls/nn/trx_encoder/tabformer_feature_encoder.py
+++ ptls/nn/trx_encoder/tabformer_feature_encoder.py
@@ -30,18 +30,15 @@ class TabFormerFeatureEncoder(nn.Module):
                        out_hidden: int = None):
         super().__init__()
 
-        out_hidden = out_hidden if out_hidden else emb_dim * (n_cols + 1)
-        self.token_sep = torch.nn.Parameter(torch.randn(1, 1, emb_dim), requires_grad=True)
+        out_hidden = out_hidden if out_hidden else emb_dim * n_cols
         encoder_layer = nn.TransformerEncoderLayer(d_model=emb_dim, nhead=n_heads, dim_feedforward=transf_feedforward_dim)
         self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=n_layers)
-
-        self.lin_proj = nn.Linear(emb_dim * (n_cols + 1), out_hidden)
+        self.lin_proj = nn.Linear(emb_dim * n_cols, out_hidden)
 
     def forward(self, input_embeds):
         embeds_shape = list(input_embeds.size())
         input_embeds = input_embeds.view([-1] + embeds_shape[-2:])
         input_embeds = input_embeds.permute(1, 0, 2)
-        input_embeds = torch.cat((input_embeds, self.token_sep.repeat(1, input_embeds.shape[1], 1)), dim=0)
         out_embeds = self.transformer_encoder(input_embeds)
         out_embeds = out_embeds.permute(1, 0, 2)
         out_embeds = out_embeds.contiguous().view(embeds_shape[0:2]+[-1])
