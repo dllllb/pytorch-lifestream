b1cc7dde48b16ce52157838a9e713558b1b70d1e
ArtyomVorobev <artemvorobev2@gmail.com>
2022-11-22 08:37:01 +0000

    fixed masker

--- ptls/frames/coles/sampling_strategies/matrix_masker.py
+++ ptls/frames/coles/sampling_strategies/matrix_masker.py
@@ -8,7 +8,6 @@ class MatrixMasker:
     (e.g. (2k-1, 2k) and (2k, 2k - 1) for k in 1 to N).
     """
     def get_masked_matrix(self, matrix, classes):
-        num_classes = len(classes) // self.split_count
         mask = (classes.unsqueeze(1) == classes.unsqueeze(0)).int()
         mask.fill_diagonal_(0)
         masked_matrix = matrix*mask
