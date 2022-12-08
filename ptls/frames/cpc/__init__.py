3ff6259a2a3d07a6f0d3a423053f0463e6252d0b
ArtyomVorobev <artemvorobev2@gmail.com>
2022-12-08 19:09:43 +0300

    fixed cpc import

--- ptls/frames/cpc/__init__.py
+++ ptls/frames/cpc/__init__.py
@@ -1,5 +1,5 @@
-from .datasets.cpc_dataset import CpcDataset, CpcIterableDataset
-from .datasets.cpc_v2_dataset import CpcV2Dataset, CpcV2IterableDataset
+from .datasets import CpcDataset, CpcIterableDataset
+from .datasets import CpcV2Dataset, CpcV2IterableDataset
 
 from .modules.cpc_module import CpcModule
 from .modules.cpc_v2_module import CpcV2Module
