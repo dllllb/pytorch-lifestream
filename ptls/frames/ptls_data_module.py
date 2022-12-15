3a9330b20be4ae03eccd964e240d8616d21934d8
Alexander <mazitovs@gmail.com>
2022-12-15 16:30:33 +0300

    Fix ptls_data_module hparams

--- ptls/frames/ptls_data_module.py
+++ ptls/frames/ptls_data_module.py
@@ -1,5 +1,6 @@
 import torch
 import pytorch_lightning as pl
+from functools import partial
 
 
 class PtlsDataModule(pl.LightningDataModule):
@@ -19,7 +20,7 @@ class PtlsDataModule(pl.LightningDataModule):
                  ):
 
         super().__init__()
-        self.save_hyperparameters()
+        self.save_hyperparameters(ignore=['train_data', 'valid_data', 'test_data'])
         
         if self.hparams.valid_num_workers is None:
             self.hparams.valid_num_workers = self.hparams.train_num_workers
@@ -32,36 +33,36 @@ class PtlsDataModule(pl.LightningDataModule):
             self.hparams.test_batch_size = self.hparams.valid_batch_size
 
         if train_data is not None:
-            self.train_dataloader = self.train_dl
+            self.train_dataloader = partial(self.train_dl, train_data)
         if valid_data is not None:
-            self.val_dataloader = self.val_dl
+            self.val_dataloader = partial(self.val_dl, valid_data)
         if test_data is not None:
-            self.test_dataloader = self.test_dl
+            self.test_dataloader = partial(self.test_dl, test_data)
             
-    def train_dl(self):
+    def train_dl(self, train_data):
         return torch.utils.data.DataLoader(
-            dataset=self.hparams.train_data,
-            collate_fn=self.hparams.train_data.collate_fn,
-            shuffle=not isinstance(self.hparams.train_data, torch.utils.data.IterableDataset),
+            dataset=train_data,
+            collate_fn=train_data.collate_fn,
+            shuffle=not isinstance(train_data, torch.utils.data.IterableDataset),
             num_workers=self.hparams.train_num_workers,
             batch_size=self.hparams.train_batch_size,
             drop_last=self.hparams.train_drop_last,
         )
 
-    def val_dl(self):
+    def val_dl(self, valid_data):
         return torch.utils.data.DataLoader(
-            dataset=self.hparams.valid_data,
-            collate_fn=self.hparams.valid_data.collate_fn,
+            dataset=valid_data,
+            collate_fn=valid_data.collate_fn,
             shuffle=False,
             num_workers=self.hparams.valid_num_workers,
             batch_size=self.hparams.valid_batch_size,
             drop_last=self.hparams.valid_drop_last,
         )
 
-    def test_dl(self):
+    def test_dl(self, test_data):
         return torch.utils.data.DataLoader(
-            dataset=self.hparams.test_data,
-            collate_fn=self.hparams.test_data.collate_fn,
+            dataset=test_data,
+            collate_fn=test_data.collate_fn,
             shuffle=False,
             num_workers=self.hparams.test_num_workers,
             batch_size=self.hparams.test_batch_size,
