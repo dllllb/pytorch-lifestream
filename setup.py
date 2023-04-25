5f025e91b6d2d7bf69386d53f5200d877f549f12
Dmitri Babaev <dmitri.babaev@gmail.com>
2023-04-25 21:18:07 +0300

    deps update and more specific deps versions (#111)

--- setup.py
+++ setup.py
@@ -24,6 +24,9 @@ setuptools.setup(
         'pytorch-lightning==1.6.*',
         'pyarrow>=7.0',
         'transformers==4.*',
-        'hydra-core>=1.1.2'
+        'hydra-core>=1.1.2',
+        'torch==1.12.*',
+        'numpy==1.23.*',
+        'torchmetrics==0.9.*',
     ],
 )
