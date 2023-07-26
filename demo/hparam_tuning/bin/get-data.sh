ab5083fda057f537b7cd196b5c5a0018a7ea4573
Dmitri Babaev <dmitri.babaev@gmail.com>
2023-07-26 19:17:13 +0300

    ptls-datasets bucket

--- demo/hparam_tuning/bin/get-data.sh
+++ demo/hparam_tuning/bin/get-data.sh
@@ -2,6 +2,6 @@
 
 mkdir data
 
-curl -OL https://storage.yandexcloud.net/di-datasets/trans-gender-2019.zip
+curl -OL https://storage.yandexcloud.net/ptls-datasets/trans-gender-2019.zip
 unzip trans-gender-2019.zip -d data
 mv trans-gender-2019.zip data/
