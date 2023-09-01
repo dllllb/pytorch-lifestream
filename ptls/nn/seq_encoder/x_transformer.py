b1dcaafce61be12c38286c66fc1f014cc45c38e6
FuTSy13 <maxkalash98@gmail.com>
2023-09-01 17:46:27 +0300

    refactor XTransformerEncoder

--- ptls/nn/seq_encoder/x_transformer.py
+++ ptls/nn/seq_encoder/x_transformer.py
@@ -15,54 +15,21 @@ class XTransformerEncoder(nn.Module):
         input_size,
         max_seq_len = None,
         attn_layers = None,
-        emb_dim = None,
-        max_mem_len = 0.,
-        shift_mem_down = 0,
-        emb_dropout = 0.,
-        post_emb_norm = False,
         num_memory_tokens = 1,
-        tie_embedding = False,
-        logits_dim = None,
-        use_abs_pos_emb = True,
-        scaled_sinu_pos_emb = False,
-        l2norm_embed = False,
-        emb_frac_gradient = 1., # GLM-130B and Cogview successfully used this, set at 0.1
-        is_reduce_sequence = True,
-        return_last = False
+        return_last = False,
+        post_emb_norm = False,
     ):
         super().__init__()
         assert isinstance(attn_layers, AttentionLayers), 'attention layers must be one of Encoder or Decoder'
 
         dim = attn_layers.dim
-        emb_dim = default(emb_dim, dim)
-        self.emb_dim = emb_dim
+        emb_dim = dim
 
-        self.max_seq_len = max_seq_len
-        self.max_mem_len = max_mem_len
-        self.shift_mem_down = shift_mem_down
-
-        self.l2norm_embed = l2norm_embed
         self.input_linear = nn.Linear(input_size, dim) 
 
-        if not (use_abs_pos_emb and not attn_layers.has_pos_emb):
-            self.pos_emb = always(0)
-        elif scaled_sinu_pos_emb:
-            self.pos_emb = ScaledSinusoidalEmbedding(emb_dim)
-        else:
-            self.pos_emb = AbsolutePositionalEmbedding(emb_dim, max_seq_len, l2norm_embed = l2norm_embed)
-
-        self.emb_frac_gradient = emb_frac_gradient # fraction of the gradient that should go to the embedding, https://arxiv.org/abs/2105.13290
-
         self.post_emb_norm = nn.LayerNorm(emb_dim) if post_emb_norm else nn.Identity()
-        self.emb_dropout = nn.Dropout(emb_dropout)
-
-        self.project_emb = nn.Linear(emb_dim, dim) if emb_dim != dim else nn.Identity()
         self.attn_layers = attn_layers
         self.norm = nn.LayerNorm(dim)
-
-        self.init_()
-
-        self.to_logits = nn.Identity() # not used
 
         self.return_last = return_last
 
@@ -72,116 +39,32 @@ class XTransformerEncoder(nn.Module):
         if num_memory_tokens > 0:
             self.memory_tokens = nn.Parameter(torch.randn(num_memory_tokens, dim))
 
-    def init_(self):
-        if self.l2norm_embed:
-            # nn.init.normal_(self.token_emb.emb.weight, std = 1e-5)
-            if not isinstance(self.pos_emb, always):
-                nn.init.normal_(self.pos_emb.emb.weight, std = 1e-5)
-            return
-
-        # nn.init.kaiming_normal_(self.token_emb.emb.weight)
-
     def forward(
         self,
         x,
-        return_embeddings = False,
-        return_logits_and_embeddings = False,
-        return_intermediates = False,
-        mask = None,
-        return_mems = False,
-        return_attn = False,
-        mems = None,
-        pos = None,
-        prepend_embeds = None,
-        sum_embeds = None,
         **kwargs
     ):
-        x_copy = x
         x = x.payload
-        b, seq_len, inp_dim, device, num_mem, emb_frac_gradient = *x.shape, x.device, self.num_memory_tokens, self.emb_frac_gradient
-        return_hiddens = return_mems | return_attn
+        b = x.shape[0]
+        num_mem = self.num_memory_tokens
 
-        # absolute positional embedding
-        external_pos_emb = exists(pos) and pos.dtype != torch.long
-        pos_emb = self.pos_emb(x, pos = pos) if not external_pos_emb else pos
-        x = self.input_linear(x) + pos_emb 
-
-        # for summing embeddings passed externally - needs this for self-conditioning in non-autoregressive training
-
-        if exists(sum_embeds):
-            x = x + sum_embeds
-
-        # post embedding norm, purportedly leads to greater stabilization
-
+        x = self.input_linear(x)
         x = self.post_emb_norm(x)
-
-        # whether to append embeds, as in PaLI, for image embeddings
-
-        if exists(prepend_embeds):
-            prepend_seq, prepend_dim = prepend_embeds.shape[1:]
-            assert prepend_dim == x.shape[-1], 'prepended embeddings need to have same dimensions as text model dimensions'
-
-            x = torch.cat((prepend_embeds, x), dim = -2)
-
-        # whether to reduce the gradient going to the embedding, from cogview paper, corroborated by GLM-130B model
-
-        if emb_frac_gradient < 1:
-            assert emb_frac_gradient > 0
-            x = x * emb_frac_gradient + x.detach() * (1 - emb_frac_gradient)
-
-        # embedding dropout
-
-        x = self.emb_dropout(x)
-
-        x = self.project_emb(x)
 
         if num_mem > 0:
             mem = repeat(self.memory_tokens, 'n d -> b n d', b = b)
             x = torch.cat((mem, x), dim = 1)
 
-            # auto-handle masking after appending memory tokens
-            if exists(mask):
-                mask = pad_at_dim(mask, (num_mem, 0), dim = -1, value = True)
-
-        if self.shift_mem_down and exists(mems):
-            mems_l, mems_r = mems[:self.shift_mem_down], mems[self.shift_mem_down:]
-            mems = [*mems_r, *mems_l]
-
-        if return_hiddens:
-            x, intermediates = self.attn_layers(x, mask = mask, mems = mems, return_hiddens = True, **kwargs)
-        else:
-            x = self.attn_layers(x, mask = mask, mems = mems, **kwargs)
+        x = self.attn_layers(x, **kwargs)
 
         x = self.norm(x)
 
         mem, x = x[:, :num_mem], x[:, num_mem:]
 
-        if return_logits_and_embeddings:
-            out = (self.to_logits(x), x)
-        elif return_embeddings:
-            out = x
-        else:
-            out = self.to_logits(x)
-
-        if return_intermediates:
-            return out, intermediates
-
-        if return_mems:
-            hiddens = intermediates.hiddens
-            new_mems = list(map(lambda pair: torch.cat(pair, dim = -2), zip(mems, hiddens))) if exists(mems) else hiddens
-            new_mems = list(map(lambda t: t[..., -self.max_mem_len:, :].detach(), new_mems))
-            return out, new_mems
-
-        if return_attn:
-            attn_maps = list(map(lambda t: t.post_softmax_attn, intermediates.attn_intermediates))
-            return out, attn_maps
-
         if self.return_last:
             return x[:,-1,:]
 
         return mem[:,0,:]
-
-
 
 class XTransformerSeqEncoder(SeqEncoderContainer):
     """SeqEncoderContainer with TransformerEncoder
@@ -214,3 +97,8 @@ class XTransformerSeqEncoder(SeqEncoderContainer):
             seq_encoder_params=seq_encoder_params,
             is_reduce_sequence=is_reduce_sequence,
         )
+
+    def forward(self, x):
+        x = self.trx_encoder(x)
+        x = self.seq_encoder(x)
+        return x
