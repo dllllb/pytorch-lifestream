5cc3930c8be15db9f50345a18310689d4693a87f
A Kostin <sasha252vns@gmail.com>
2023-12-25 23:25:04 +0000

    cluster off

--- ptls/frames/coles/__init__.py
+++ ptls/frames/coles/__init__.py
@@ -2,6 +2,6 @@ from .coles_dataset import ColesDataset, ColesIterableDataset
 from .cluster_dataset import ClusterDataset, ClusterIterableDataset
 from .coles_supervised_dataset import ColesSupervisedDataset, ColesSupervisedIterableDataset
 from .coles_module import CoLESModule
-from .cluster_module import ClusterModule, ClusterCallback
+#from .cluster_module import ClusterModule, ClusterCallback
 from .coles_supervised_module import ColesSupervisedModule
 
