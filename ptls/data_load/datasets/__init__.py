2bb5e740b3bff83fc16a52c35d7c5e532d78e486
Fw9wef <Sasha252VNS@gmail.com>
2024-03-10 01:41:28 +0300

    syndata

--- ptls/data_load/datasets/__init__.py
+++ ptls/data_load/datasets/__init__.py
@@ -5,6 +5,6 @@ from .memory_dataset import MemoryMapDataset, MemoryIterableDataset
 from .parquet_dataset import ParquetFiles, ParquetDataset
 from .parquet_file_scan import parquet_file_scan
 from .dataloaders import inference_data_loader
-from .synthetic_dataset import SyntheticDataset
+from .synthetic_dataset import SyntheticDataset, SyntheticDatasetWriter, MonoTargetSyntheticDatasetWriter
 from .synthetic_utils import State, CategoryFeature, ConstFeature, FloatFeature
 from .synthetic_client import SyntheticClient, SimpleSchedule, Config, SphereSampler, PlaneClassAssigner
