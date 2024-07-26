6a6c65c1ef8bb6200c4942ab9e4ec59122ae379f
v1docq <ir@raxeltelematics.com>
2024-07-26 18:55:55 +0300

    change dask backed for multithread

--- ptls/data_load/iterable_processing/seq_len_filter.py
+++ ptls/data_load/iterable_processing/seq_len_filter.py
@@ -1,5 +1,4 @@
 import logging
-from itertools import compress
 from ptls.data_load import IterableProcessingDataset
 
 logger = logging.getLogger(__name__)
@@ -22,25 +21,19 @@ class SeqLenFilter(IterableProcessingDataset):
         self._sequence_col = sequence_col
         self._seq_len_col = seq_len_col
 
-    def _valid_seq_len(self, seq_len):
-        min_len_check = seq_len > self._min_seq_len if self._min_seq_len is not None else True
-        max_len_check = seq_len < self._max_seq_len if self._max_seq_len is not None else True
+    def _valid_seq_len(self, seq_feature):
+        if self.is_seq_feature(seq_feature):
+            _ = len(seq_feature)
+            min_len_check = _ > self._min_seq_len if self._min_seq_len is not None else True
+            max_len_check = _ < self._max_seq_len if self._max_seq_len is not None else True
         return all([min_len_check, max_len_check])
 
     def transform(self, features):
-        valid_seq_len = self.get_len(features)
-        return features if valid_seq_len else None
+        return features if self.get_len(features[list(features.keys())[0]]) else None
 
-    def get_sequence_col(self, rec):
-        iter_record = list(iter(rec))
-        iter_record = list(compress(iter_record, [self.is_seq_feature(i) for i in iter_record]))
-        if len(iter_record) == 0:
-            raise ValueError(f'Can not find field with sequence from record: {rec}')
-        else:
-            return all([self._valid_seq_len(len(feature)) for feature in iter_record])
+    def get_sequence_col(self, rec: dict):
+        #filter_func = list(filter(lambda x: 'CI' in rec.get(x), rec))
+        return all([self._valid_seq_len(feature) for feature in rec.values()])
 
     def get_len(self, rec):
-        if self._seq_len_col is not None:
-            return rec[self._seq_len_col]
-        else:
-            return self.get_sequence_col(rec)
+        return rec[self._seq_len_col] if self._seq_len_col is not None else self.get_sequence_col(rec)
