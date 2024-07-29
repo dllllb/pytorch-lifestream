cbaa32b8ba6631e902819ba5306e7c1b953b3cfe
v1docq <ir@raxeltelematics.com>
2024-07-29 16:13:32 +0300

    coles dataset, coles model optimise

--- ptls/frames/coles/coles_dataset.py
+++ ptls/frames/coles/coles_dataset.py
@@ -1,7 +1,9 @@
 from functools import reduce
 from operator import iadd
 
+import joblib
 import torch
+from joblib import Parallel, delayed
 
 from ptls.data_load.feature_dict import FeatureDict
 from ptls.data_load.utils import collate_feature_dict
@@ -44,10 +46,18 @@ class ColesDataset(FeatureDict, torch.utils.data.Dataset):
         for feature_arrays in self.data:
             yield self.get_splits(feature_arrays)
 
+    def _create_split_subset(self, idx, feature_arrays):
+        return {k: v[idx] for k, v in feature_arrays.items()}
+
     def get_splits(self, feature_arrays):
         local_date = feature_arrays[self.col_time]
         indexes = self.splitter.split(local_date)
-        return [{k: v[ix] for k, v in feature_arrays.items() if self.is_seq_feature(k, v)} for ix in indexes]
+        with joblib.parallel_backend(backend='threading'):
+            parallel = Parallel(verbose=1)
+            result_dict = parallel(delayed(self._create_split_subset)(idx, feature_arrays)
+                                   for idx in indexes)
+
+        return result_dict
 
     @staticmethod
     def collate_fn(batch):
