0af07b2cb05e300e7f9c815afb11d721e65a4279
al <Sasha252VNS@gmail.com>
2024-07-30 06:42:15 +0300

    multi coles

--- demo/multicoles_test_tools/downstream_test.py
+++ demo/multicoles_test_tools/downstream_test.py
@@ -77,7 +77,7 @@ def solve_downstream(train_data, test_data, metric, conf_path='./config.hocon'):
             pred_y = clf.predict(test_data.drop('target', axis=1))
             test_score = accuracy_score(y_pred=pred_y, y_true=test_data['target'])
         elif metric =='rocauc':
-            pred_y = clf.predict_proba(test_data.drop('target', axis=1))[:, 1]
+            pred_y = clf.predict_proba(test_data.drop('target', axis=1))#[:, 1]
             print(pred_y)
             print(test_data['target'])
             test_score = roc_auc_score(y_score=pred_y, y_true=test_data['target'])
