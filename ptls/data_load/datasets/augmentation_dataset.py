cd10c1a519f0d05e301ef4ea1227f06d53f2388d
technocreep <aspirantvadim@gmail.com>
2024-09-06 18:11:57 +0200

    pep8 update for augmentation_dataset.py

--- ptls/data_load/datasets/augmentation_dataset.py
+++ ptls/data_load/datasets/augmentation_dataset.py
@@ -14,7 +14,7 @@ class AugmentationDataset(torch.utils.data.Dataset):
     def __getitem__(self, item):
         rec = self.data[item]
         return self.f_augmentations(rec)
-    
+
 
 class AugmentationIterableDataset(torch.utils.data.IterableDataset):
     """
@@ -22,11 +22,12 @@ class AugmentationIterableDataset(torch.utils.data.IterableDataset):
     'data' argument must be `torch.utils.data.IterableDataset`
 
     """
+
     def __init__(self, data, f_augmentations: list = None):
         assert isinstance(data, torch.utils.data.IterableDataset)
         self.data = data
         self.f_augmentations = AugmentationChain(f_augmentations)
-    
+
     def __iter__(self):
         for rec in self.data:
             yield self.f_augmentations(rec)
