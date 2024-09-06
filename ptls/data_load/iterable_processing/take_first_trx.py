deff80f421c2807c266c385e73adaf848b59e1ce
technocreep <aspirantvadim@gmail.com>
2024-09-06 14:31:06 +0200

    updated docstrings, type hints, and deleted duplicated fragments

--- ptls/data_load/iterable_processing/take_first_trx.py
+++ ptls/data_load/iterable_processing/take_first_trx.py
@@ -4,17 +4,19 @@ from ptls.data_load import IterableProcessingDataset
 class TakeFirstTrx(IterableProcessingDataset):
     def __init__(self, take_first_fraction=0.5, seq_len_col=None, sequence_col=None):
         """
-        Trim sequences by taking the first `take_first_fraction` portion of each sequence.
-        The remaining part is discarded.
+        Filter sequences by length. Drop sequences shorter than `min_seq_len` and longer than `max_seq_len`.
 
         Args:
-            take_first_fraction: Controls the fraction of the sequence to keep.
-                                 EXAMPLE: take_first_fraction=0.75 -> the first 75% of the user's
-                                          transactions will be kept, and the last 25% will be discarded.
-            seq_len_col:  Field where the sequence length is stored. If None, `target_col` is used.
-            sequence_col: Field for detecting sequence length. If None, any iterable field will be used.
+            take_first_fraction: control the fraction of transactions to keep
+                                 EXAMPLE: take_first_fraction=0.75 -> the last 0.25 of all user
+                                          transactions will be chosen as user target distribution
+                                          and therefore will be cutted off
+            seq_len_col:  field where sequence length stored, if None, `target_col` used
+            sequence_col: field for sequence length detection, if None, any
+                          iterable field will be used
         """
         super().__init__()
+
         self._take_first_fraction = take_first_fraction
         self._sequence_col = sequence_col
         self._seq_len_col = seq_len_col
