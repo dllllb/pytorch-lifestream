5573dbf66e0661a3950f1af15e338473c7338e51
ismonnar <47676856+ismonnar@users.noreply.github.com>
2024-10-14 17:49:50 +0300

    Delete scenario_alpha_battle directory

--- scenario_alpha_battle/bin/get-data.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-git clone --depth 1 https://huggingface.co/datasets/dllllb/alfa-scoring-trx data
-
-gunzip -f data/*.csv.gz
-
-mv data/train_transactions data/train_transactions_contest.parquet
-mv data/test_transactions data/test_transactions_contest.parquet
