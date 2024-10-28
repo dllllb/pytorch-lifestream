4fedb963b143288c35f64bf91bfc602d81ff04cd
Leon_Strelkov <103892559+leostre@users.noreply.github.com>
2024-10-28 15:37:58 +0300

    Update fc_train.py

--- ptls/fedcore_compression/fc_train.py
+++ ptls/fedcore_compression/fc_train.py
@@ -1,13 +1,16 @@
-import hydra
-from fc_utils import fedcore_fit, get_experimental_setup
+"""Module for training and compression using FedCore"""
 import logging
+
+import hydra
+from omegaconf import DictConfig
 from pathlib import Path
 import pytorch_lightning as pl
 import torch
-from omegaconf import DictConfig
+
+from ptls.fedcore_compression.fc_utils import fedcore_fit, get_experimental_setup
+
 
 logger = logging.getLogger(__name__)
-
 
 @hydra.main(version_base='1.2', config_path=None)
 def main(conf: DictConfig):
@@ -20,16 +23,15 @@ def main(conf: DictConfig):
     save_path = f'composition_results/{setup_name}'
     Path(save_path).mkdir(parents=True, exist_ok=True)
     experiment_setup['output_folder'] = save_path
-    if hasattr(conf, 'limit_train_batches'):
-        experiment_setup['common']['max_train_batch'] = conf.limit_train_batches
-    if hasattr(conf, 'limit_valid_batches'):
-        experiment_setup['common']['max_calib_batch'] = conf.limit_valid_batches
-    if hasattr(conf, 'need_pretrain'):
-        experiment_setup['need_pretrain'] = conf.need_pretrain
+    experiment_setup['common']['batch_limit'] = conf.get('limit_train_batches', None)
+    experiment_setup['common']['calib_batch_limit'] = conf.get('limit_valid_batches', None)
+    experiment_setup['need_fedot_pretrain'] = conf.get('need_fedot_pretrain', False)
+    experiment_setup['need_evo_opt'] = conf.get('need_evo_opt', False)
+    experiment_setup['distributed_compression'] = conf.get('distributed_compression', False)
         
     # instantiate model
     model = hydra.utils.instantiate(conf.pl_module)
-    if 'pretrained' in conf:
+    if 'pretrained' in conf and conf.pretrained:
         model.seq_encoder = torch.load(conf.pretrained)
 
     # instantiate data_module
