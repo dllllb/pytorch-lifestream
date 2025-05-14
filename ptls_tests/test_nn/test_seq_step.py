c1dc21b86c5372963a46d4a28cc8ff84a44df0a8
Artem Sakhno <asaxno@bk.ru>
2025-05-14 10:49:05 +0300

    add test for last_step_encoder

--- ptls_tests/test_nn/test_seq_step.py
+++ ptls_tests/test_nn/test_seq_step.py
@@ -2,7 +2,7 @@ import torch
 
 from ptls.nn import TimeStepShuffle
 from ptls.data_load.padded_batch import PaddedBatch
-from ptls.nn.seq_step import SkipStepEncoder
+from ptls.nn.seq_step import SkipStepEncoder, LastStepEncoder
 
 
 def test_timestep_shuffle():
@@ -22,3 +22,25 @@ def test_skip_step_encoder():
     res = SkipStepEncoder(3)(PaddedBatch(t, [10, 9, 8, 7, 3, 2, 1, 0]))
 
     assert res.payload.shape == (8, 4, 2)
+
+def test_last_step_encoder_padding():
+    t = torch.tensor([
+        [[1, 1], [2, 2], [3, 3], [0, 0], [0, 0]],  
+        [[4, 4], [5, 5], [0, 0], [0, 0], [0, 0]],  
+        [[6, 6], [0, 0], [0, 0], [0, 0], [0, 0]],  
+    ])
+    
+    seq_lens = torch.tensor([3, 2, 1])
+    encoder = LastStepEncoder()
+    
+    
+    result = encoder(PaddedBatch(t, seq_lens))
+    
+    
+    expected = torch.tensor([
+        [3, 3],  
+        [5, 5],  
+        [6, 6],  
+    ])
+
+    assert torch.allclose(result, expected), "LastStepEncoder wrong indexation"
